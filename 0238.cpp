6d0d2cda7b92ccaee179a707ea856e84943783fd
Jacob Pfeifer <jakepf00@gmail.com>
2024-01-17 08:55:45 -0600

    Completed problem with O(1) extra space complexity

--- 0238.cpp
+++ 0238.cpp
@@ -1,3 +1,21 @@
+class Solution { // Solution with O(1) extra space complexity
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans;
+        ans.resize(nums.size());
+        ans[0] = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int postfix = 1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            postfix = postfix * nums[i+1];
+            ans[i] *= postfix;
+        }
+        return ans;
+    }
+};
+
 class Solution { // Better solution using pre and postfix
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
